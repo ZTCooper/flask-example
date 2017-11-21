74420bc797b0e4cf432458b6b110cb08a7c6f902
ZTCooper <ztcooper@outlook.com>
2017-11-21 14:21:26 +0800

    views, login and logout

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -45,3 +45,46 @@ def close_db(error):
 	if hasattr(g, 'sqlite_db'):
 		g.sqlite_db.close()
 
+#显示条目
+@app.route('/')
+def show_entries():
+	db = get_db()
+	cur = db.execute('select title, text from entries order by id desc')
+																#id降序
+	entries = cur.fetchall()
+	return render_template('show_entries.html', entries = entries)
+
+#添加条目
+@app.route('/add', methods = ['POST'])
+def add_entry():
+	if not session.get('logged_in'):	#用户未登入
+		abort(401)		#放弃请求并返回错误代码
+	db = get_db()
+	db.execute('insert into entries (title, text) values (?, ?)',
+				[request.form['title'], request.form['text']])
+				#使用?标记构建sql语句
+	db.commit()
+	flash('New entry was successfully posted')
+	return redirect(url_for('show_entries'))
+
+#login
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	error = None
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME']:
+			error = 'Invalid username'
+		elif request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid password'
+		else:
+			session['logged_in'] = True
+			flash('You were logged in')
+			return redirect(url_for('show_entries'))
+	return render_template('login.html', error = error)
+
+#logout
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('show_entries'))
